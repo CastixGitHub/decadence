5187310e8e193b76fd9d181f0e297639049810c7
castix <castix@onenetbeyond.org>
2025-12-02 23:24:50 +0100

    Make all the thing GLSL
    So I wanted to use the geometry shader to draw the boxes
    planning to rawdog the pyglet mouse handing afterwards
    
    This still is dumb when it comes to drawing the "on" state
    Let's see how to fix it

---
+++ allgl.py
@@ -0,0 +1,188 @@
+
+import pyglet
+from pyglet.window import Window
+from pyglet.text import Label
+from pyglet.graphics.shader import Shader, ShaderProgram
+
+pyglet.options['debug_gl'] = True
+pyglet.options['debug_gl_trace'] = True
+pyglet.options['debug_gl_trace_args'] = True
+pyglet.options['debug_glsl'] = True
+
+window = pyglet.window.Window(caption='decadence', width=600, height=400)
+#wel = pyglet.window.event.WindowEventLogger()
+#window.push_handlers(wel)
+
+class SquareButton:
+    vertex_source = """
+#version 330 core
+in vec2 myposition;
+in float mysize;
+
+uniform WindowBlock
+{
+    mat4 projection;
+    mat4 view;
+} window;
+
+out float geosize;
+out vec2 center;
+void main()
+{
+    gl_Position = window.projection * window.view * vec4(myposition, 0.0, 1.0);
+    center = gl_Position.xy;
+    geosize = mysize;
+}
+    """
+    geometry_source = """
+#version 330 core
+layout (points) in;
+layout (line_strip, max_vertices = 5) out;
+
+uniform WindowBlock
+{
+    mat4 projection;
+    mat4 view;
+} window;
+
+in float geosize[];
+
+out vec3 vertex_color;
+
+
+void emit_origin(vec3 color) {
+    gl_Position = gl_in[0].gl_Position;
+    vertex_color = color;
+    EmitVertex();
+}
+
+void emit_offset(float x, float y, vec3 color) {
+    gl_Position = gl_in[0].gl_Position
+        + window.projection * vec4(x, y, .0, .0);
+    vertex_color = color;
+    EmitVertex();
+}
+
+void main() {
+    float mysize = geosize[0];
+    emit_origin(vec3(1., .0, .0));
+    emit_offset(mysize, 0., vec3(.0, 1.0, .0));
+    emit_offset(mysize, mysize, vec3(.0, 1.0, .0));
+    emit_offset(0., mysize, vec3(.0, .0, 1.0));
+    emit_origin(vec3(1., .0, 1.0));
+    EndPrimitive();
+}
+    """
+    fragment_source = """
+#version 330 core
+uniform float time;
+in vec3 vertex_color;
+void main()
+{
+    gl_FragColor = vec4(vertex_color, 1.0);//vec4(1., 0., 0., 1.);//vec4(abs(sin(time)), 1., 1., 1.0);
+}
+    """
+
+    geometry_source_on = """
+#version 330 core
+layout (points) in;
+layout (points, max_vertices=200) out;
+
+uniform WindowBlock
+{
+    mat4 projection;
+    mat4 view;
+} window;
+
+in float geosize[];
+
+void main() {
+    vec4 pos = gl_in[0].gl_Position;
+    for (int i = 5; i < geosize[0] - 5; i++) {
+        for (int j = 5; j < geosize[0] - 5; j++) {
+            gl_Position = pos + window.projection *
+                vec4(pos.x - geosize[0]/2 + i, pos.y - geosize[0]/2 + j, 0., 0.);
+            EmitVertex();
+        }
+    }
+    EndPrimitive();
+}
+    """
+
+    fragment_source_on = """
+#version 330 core
+
+in vec2 center;
+void main()
+{
+    gl_FragColor = vec4(1., 1., 0., 1.);
+}
+    """
+
+    def __init__(self):
+        self.program_square = pyglet.gl.current_context.create_program(
+            (self.vertex_source, 'vertex'),
+            (self.geometry_source, 'geometry'),
+            (self.fragment_source, 'fragment'),
+        )
+        self.program_on = pyglet.gl.current_context.create_program(
+            (self.vertex_source, 'vertex'),
+            (self.geometry_source_on, 'geometry'),
+            (self.fragment_source_on, 'fragment'),
+        )
+        self.boxes = []
+
+    def add(self, x, y, size, on):
+        self.boxes.append((x, y, size, on))
+
+    def compile(self, batch):
+        count = len(self.boxes)
+        coords = ('f', (_coords := []))
+        size = ('f', (_size := []))
+        for box in self.boxes:
+            _coords.extend((box[0], box[1]))
+            _size.extend([box[2]])
+        self.square_vertexes = self.program_square.vertex_list(
+            count,
+            pyglet.gl.GL_POINTS,
+            batch=batch,
+            myposition=coords,
+            mysize=size,
+        )
+        count = 0
+        coords = ('f', (_coords := []))
+        size = ('f', (_size := []))
+        for box in self.boxes:
+            if box[3]:  # on
+                count += 1
+                _coords.extend((box[0] + box[2] / 2, box[1] + box[2] / 2))
+                _size.extend([box[2]])
+        self.on_vertexes = self.program_on.vertex_list(
+            count,
+            pyglet.gl.GL_POINTS,
+            batch=batch,
+            myposition=coords,
+            mysize=size,
+        )
+
+from time import time
+
+square_button = SquareButton()
+square_button.add(20., 25., 20., False)
+square_button.add(50., 50., 20., True)
+square_button.add(200., 200., 20., False)
+square_button.add(100., 125., 30., True)
+batch = pyglet.graphics.Batch()
+square_button.compile(batch)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    #vertex_list.time = time()
+    batch.draw()
+    #print(vertex_list.myposition[:])
+    #vertex_list.myposition[:] = [*map(lambda x: x + 1, vertex_list.myposition[:])]
+    #breakpoint()
+
+pyglet.app.run(.05)
